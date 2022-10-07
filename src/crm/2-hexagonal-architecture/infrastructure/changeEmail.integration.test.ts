542ce235771150dd679936e6b1bef9318ac98f06
Roman Mkrtchian <mkrtchia@gmail.com>
2022-10-07 02:41:24 +0200

    test: remove unuseful integration tests from CRM project's hexagonal example (#16)

--- src/crm/2-hexagonal-architecture/infrastructure/changeEmail.integration.test.ts
+++ src/crm/2-hexagonal-architecture/infrastructure/changeEmail.integration.test.ts
@@ -1,4 +1,3 @@
-import { UserType } from "../domain/types";
 import { closeDB, db, deleteDB, initializeDB } from "./Database";
 import { UserController } from "./UserController";
 
@@ -12,77 +11,13 @@ afterEach(async () => {
 });
 
 it("changes email from non corporate to corporate", async () => {
-  await testChangeEmail({
-    initialEmail: "user@gmail.com",
-    newEmail: "user@mycorp.com",
-    initialType: "customer",
-    newType: "employee",
-    initialNumberOfEmployees: 0,
-    newNumberOfEmployees: 1,
-  });
-});
-
-it("changes email from corporate to non corporate", async () => {
-  await testChangeEmail({
-    initialEmail: "user@mycorp.com",
-    newEmail: "user@gmail.com",
-    initialType: "employee",
-    newType: "customer",
-    initialNumberOfEmployees: 1,
-    newNumberOfEmployees: 0,
-  });
-});
-
-it("does not change anything when the email is the same", async () => {
-  await testChangeEmail({
-    initialEmail: "user@mycorp.com",
-    newEmail: "user@mycorp.com",
-    initialType: "employee",
-    newType: "employee",
-    initialNumberOfEmployees: 1,
-    newNumberOfEmployees: 1,
-  });
-});
-
-it("changes an email for a corporate user", async () => {
-  await testChangeEmail({
-    initialEmail: "user@mycorp.com",
-    newEmail: "new@mycorp.com",
-    initialType: "employee",
-    newType: "employee",
-    initialNumberOfEmployees: 1,
-    newNumberOfEmployees: 1,
-  });
-});
-
-it("changes an email for a non corporate user", async () => {
-  await testChangeEmail({
-    initialEmail: "user@gmail.com",
-    newEmail: "new@gmail.com",
-    initialType: "customer",
-    newType: "customer",
-    initialNumberOfEmployees: 0,
-    newNumberOfEmployees: 0,
-  });
-});
-
-async function testChangeEmail({
-  initialEmail,
-  newEmail,
-  initialType,
-  newType,
-  companyDomain = "mycorp.com",
-  initialNumberOfEmployees,
-  newNumberOfEmployees,
-}: {
-  initialEmail: string;
-  newEmail: string;
-  initialType: UserType;
-  newType: UserType;
-  companyDomain?: string;
-  initialNumberOfEmployees: number;
-  newNumberOfEmployees: number;
-}) {
+  const initialEmail = "user@gmail.com";
+  const newEmail = "user@mycorp.com";
+  const initialType = "customer";
+  const newType = "employee";
+  const companyDomain = "mycorp.com";
+  const initialNumberOfEmployees = 0;
+  const newNumberOfEmployees = 1;
   await db.run(
     `INSERT INTO Company (domainName, numberOfEmployees) VALUES ('${companyDomain}', ${initialNumberOfEmployees})`
   );
@@ -102,4 +37,4 @@ async function testChangeEmail({
     "SELECT numberOfEmployees FROM Company"
   );
   expect(companyData?.numberOfEmployees).toBe(newNumberOfEmployees);
-}
+});
