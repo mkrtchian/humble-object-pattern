21cf0acb6535dc72210da6db1890d0a6a7b58e84
Roman Mkrtchian <mkrtchia@gmail.com>
2022-10-10 22:55:48 +0200

    test(crm): add checks for sending a message in the message bus in the hexagonal example

--- src/crm/2-hexagonal-architecture/infrastructure/MessageBus.ts
+++ src/crm/2-hexagonal-architecture/infrastructure/MessageBus.ts
@@ -1,14 +1,23 @@
-class IBus {
-  send(message: string) {
-    console.log(message);
-    // TODO: add a real message broker
+export interface IBus {
+  send(message: string): void;
+}
+
+export class MessageBus {
+  private bus: IBus;
+  constructor(bus?: IBus) {
+    this.bus = bus ?? new Bus();
+  }
+
+  sendEmailChangedMessage(userId: number, newEmail: string) {
+    this.bus.send(
+      `Subject: USER; Type: EMAIL CHANGED; Id: ${userId}; NewEmail: ${newEmail}`
+    );
   }
 }
 
-const bus: IBus = new IBus();
-
-export function sendEmailChangedMessage(userId: number, newEmail: string) {
-  bus.send(
-    `Subject: USER; Type: EMAIL CHANGED; Id: ${userId}; NewEmail: ${newEmail}`
-  );
+class Bus implements IBus {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  send(message: string) {
+    // TODO: add a real message broker
+  }
 }
--- src/crm/2-hexagonal-architecture/infrastructure/changeEmail.integration.test.ts
+++ src/crm/2-hexagonal-architecture/infrastructure/changeEmail.integration.test.ts
@@ -1,4 +1,5 @@
 import { closeDB, db, deleteDB, initializeDB } from "./Database";
+import { MessageBus } from "./MessageBus";
 import { UserController } from "./UserController";
 
 beforeEach(async () => {
@@ -11,30 +12,26 @@ afterEach(async () => {
 });
 
 it("changes email from non corporate to corporate", async () => {
-  const initialEmail = "user@gmail.com";
-  const newEmail = "user@mycorp.com";
-  const initialType = "customer";
-  const newType = "employee";
-  const companyDomain = "mycorp.com";
-  const initialNumberOfEmployees = 0;
-  const newNumberOfEmployees = 1;
   await db.run(
-    `INSERT INTO Company (domainName, numberOfEmployees) VALUES ('${companyDomain}', ${initialNumberOfEmployees})`
+    `INSERT INTO Company (domainName, numberOfEmployees) VALUES ('mycorp.com', 0)`
   );
   await db.run(
-    `INSERT INTO User (id, email, type) VALUES (1, '${initialEmail}', '${initialType}')`
+    `INSERT INTO User (id, email, type) VALUES (1, 'user@gmail.com', 'customer')`
   );
-  const sut = new UserController();
+  const busMock = { send: jest.fn() };
+  const sut = new UserController(new MessageBus(busMock));
 
-  await sut.changeEmail(1, newEmail);
+  await sut.changeEmail(1, "user@mycorp.com");
 
   const userData: { email: string; type: string } | undefined = await db.get(
     "SELECT * FROM User WHERE id = 1"
   );
-  expect(userData?.email).toBe(newEmail);
-  expect(userData?.type).toBe(newType);
+  expect(userData?.email).toBe("user@mycorp.com");
+  expect(userData?.type).toBe("employee");
   const companyData: { numberOfEmployees: number } | undefined = await db.get(
     "SELECT numberOfEmployees FROM Company"
   );
-  expect(companyData?.numberOfEmployees).toBe(newNumberOfEmployees);
+  expect(companyData?.numberOfEmployees).toBe(1);
+  expect(busMock.send).toHaveBeenCalledTimes(1);
+  expect(busMock.send).toHaveBeenCalledWith(expect.toStartWith("Subject"));
 });
